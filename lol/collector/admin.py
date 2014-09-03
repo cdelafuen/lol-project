64b6336f03712f50c6a24a7f43a5cd953052d52e
cdelafuente <cdelafuente@paradigmatecnologico.com>
2014-09-03 15:36:53 +0200

    Added fighting type

--- lol/collector/admin.py
+++ lol/collector/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Champion
+from .models import Champion, FightingType, ChampionFightingType
+
+
+class ChampionFightingTypeInline(admin.TabularInline):
+    model = ChampionFightingType
+    extra = 1
 
 
 class ChampionAdmin(admin.ModelAdmin):
-    list_display = ('champion_id', 'name', 'sex', 'partype')
+    list_display = ('name', 'champion_id',  'sex', 'partype')
+    inlines = [ChampionFightingTypeInline]
 
 admin.site.register(Champion, ChampionAdmin)
+admin.site.register(FightingType)
--- lol/collector/models.py
+++ lol/collector/models.py
@@ -4,6 +4,13 @@ from django.db import models
 import requests
 
 
+class FightingType(models.Model):
+    type = models.CharField(max_length=45, primary_key=True)
+
+    def __unicode__(self):
+        return self.type
+
+
 class Champion(models.Model):
     champion_id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=45)
@@ -13,9 +20,15 @@ class Champion(models.Model):
     difficulty = models.PositiveSmallIntegerField()
     sex = models.CharField(max_length=45)
     partype = models.CharField(max_length=45)
+    fighting_types = models.ManyToManyField(FightingType, through='ChampionFightingType', blank=True, null=True)
 
     def __unicode__(self):
         return self.name
+
+
+class ChampionFightingType(models.Model):
+    champion = models.ForeignKey(Champion)
+    fighting_type = models.ForeignKey(FightingType)
 
 
 class Summoner(models.Model):
@@ -60,10 +73,14 @@ def update_champions():
     if response.status_code == 200:
         champions = json.loads(response.content)
         for champion in champions['data']:
-            print champions['data'][champion]
             basic_params = champions['data'][champion]['info']
-            Champion.objects.get_or_create(champion_id=champions['data'][champion]['key'],
-                                           name=champions['data'][champion]['name'],
-                                           attack=basic_params['attack'], defense=basic_params['defense'],
-                                           magic=basic_params['magic'], difficulty=basic_params['difficulty'],
-                                           partype=champions['data'][champion]['partype'])
+            c, created = Champion.objects.get_or_create(champion_id=champions['data'][champion]['key'],
+                                                        name=champions['data'][champion]['name'],
+                                                        attack=basic_params['attack'], defense=basic_params['defense'],
+                                                        magic=basic_params['magic'], difficulty=basic_params['difficulty'],
+                                                        partype=champions['data'][champion]['partype'])
+            tags = champions['data'][champion]['tags']
+            for type in tags:
+                ft, created = FightingType.objects.get_or_create(type=type)
+                ChampionFightingType.objects.get_or_create(champion=c, fighting_type=ft)
+                print c, type, ft, created
